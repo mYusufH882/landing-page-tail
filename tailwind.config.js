ce8cdc2bf1f3b99c15b477b5e90a86b61be694f7
mYusufH882 <myusufh882@gmail.com>
2021-01-11 11:32:41 +0700

    First commit in work

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { colors } = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  // prefix: "lus-",
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
